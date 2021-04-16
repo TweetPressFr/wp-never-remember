7e234aece3fee198ecf09ae09342eec49df3bfd7
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-16 15:59:47 +0200

    fix indentation

--- wp-never-remember.php
+++ wp-never-remember.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
  * Plugin name: WP Never Remember
  * Author: Julien Maury
  * Author URI: https://www.julien-maury.dev
- * Version: 0.1
+ * Version: 0.2
  * Description: I don't encourage the use of this plugin. I say I prefer this way. Nothing more. Maybe I'm wrong maybe I'm not. Use it "as is" with caution :)
  */
 
-if ( ! function_exists( 'add_action' ) ) {
-	die('~(|)~');
+if (!function_exists('add_action')) {
+    die('~(|)~');
 }
 
-add_action( 'login_form', function (){
-	ob_start();
-}, 999 );
+add_action('login_form', function () {
+    ob_start();
+}, 999);
 
-add_action( 'login_footer', function() {
-	echo preg_replace( '#<p class="forgetmenot">.+</p>#', '', ob_get_clean() );
-} );
+add_action('login_footer', function () {
+    echo preg_replace('#<p class="forgetmenot">.+</p>#', '', ob_get_clean());
+});
